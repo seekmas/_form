4d6ac53cede2225a8350895dab3f105df69eeea2
seekmas <seekmas@msn.cn>
2015-01-28 15:06:59 +0800

    add validates

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,3 +1,7 @@
 class Check < ActiveRecord::Base
   belongs_to :activity
+
+  validates :name , :presence => true
+  validates :phone , :presence => true
+  validates :email , :presence => true
 end
